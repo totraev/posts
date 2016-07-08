d180c6671beaaf9c9ecf90b308629b86265ce6bd
Nika <veronika_tyan92@list.ru>
2016-07-08 14:06:12 +0500

    08/07/16

---
+++ client/app/movies/movies.filter.js
@@ -0,0 +1,14 @@
+app.filter("evenItems", function(){
+  return function(item, arg1){
+    console.log(item);
+    if(item instanceof Array){
+      var even = [];
+      for (var i = 0; i < item.length; i++) {
+        if (i%arg1 == 0) {
+          even.push(item[i]);
+        }
+      }
+      return even;
+    } else return item;
+  }
+})
